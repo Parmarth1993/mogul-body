c1ece83befd1316d8b9f267ad7d257a19dd57886
Randhir Singh <randhir@xirgosoft.com>
2019-11-21 12:29:49 +0530

    Fixed Validation and added admin dashboard

--- app/Http/Controllers/Admin/DashboardController.php
+++ app/Http/Controllers/Admin/DashboardController.php
@@ -49,13 +49,57 @@ class DashboardController extends Controller
 
     }
 
-    public function updateprofile(){
+    public function updateprofile(Request $request){
+
+        $this->validate($request, [
+            'first_name' => 'required|max:255',
+            'last_name' => 'required|max:255',
+        ]);
+
+        /**
+         * storing the input fields name & email in variable $input
+         * type array
+         **/
+        $input = $request->only('first_name','last_name');
+        /**
+         * fetching the user model
+         */
+        $user = Auth::user();
+
+        /**
+         * Accessing the update method and passing in $input array of data
+         **/
+        $user->update(['first_name' => $input['first_name'],
+                       'last_name' => $input['last_name']]);
+
+        /**
+         * Change password if not blank
+
+        /**
+         * after everything is done return them pack to /profile/ uri
+         **/
+        return redirect('/admin/profile')->with('success', 'Profile has been updated.');
 
     }
 
-     public function updatepassword(){
+    public function updatepassword(Request $request){
+        
+         $this->validate($request, [
+            'new_password' => 'required|max:255',
+            'confirm_password' => 'required|max:255',
+        ]);
 
+          $input = $request->only('new_password','confirm_password');
+
+          $user = Auth::user();
+
+          if(isset($input['new_password']) && $input['new_password'] != '' && $input['new_password'] == $input['confirm_password']) {
+            $update = $user->update(['password' => bcrypt($input['new_password'])]);
+            return redirect('/admin/profile')->with('success', 'Password has been changed.');
+        } else{ 
+
+            return redirect('/admin/profile')->with('error', 'Password and confirm password do not match.');
+        }
     }
-
 
 }
--- resources/views/admin/profile.blade.php
+++ resources/views/admin/profile.blade.php
@@ -4,43 +4,99 @@
         <div class="container-fluid">
             <div class="row">
                 <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
-                        <h2>My Profile</h2>
-                        {!! Form::open(['url' => route('updateprofile'), 'class' => 'update-profile']) !!}
-                            {{ csrf_field() }}
-                          <div class="form-group">
-                            <label for="first_name">First Name:</label>
-                            <input type="text" class="form-control" id="first_name" name="first_name" value="{{$user->first_name}}">
-                          </div>
-                          <div class="form-group">
-                            <label for="last_name">Last Name:</label>
-                            <input type="text" class="form-control" id="last_name" name="last_name" value="{{$user->last_name}}">
-                          </div>
-                          <div class="form-group">
-                            <label for="email">Email:</label>
-                            <input type="email" class="form-control" id="email" name="email" value="{{$user->email}}" disabled>
-                          </div>
-                          <button type="submit" class="btn btn-primary">Update Profile</button>
-                        </form>
-                </div><br />
-                <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
-                       <br />  <h2>Change Password</h2>
-                        {!! Form::open(['url' => route('updatepassword'), 'class' => 'update-password']) !!}
-                            {{ csrf_field() }}
-                          <div class="form-group">
-                            <label for="pwd">Old Password:</label>
-                            <input name="password" type="password" class="form-control" id="password">
-                          </div>
-                          <div class="form-group">
-                            <label for="pwd">New Password:</label>
-                            <input name="new_password" type="password" class="form-control" id="new_password">
-                          </div>
-                          <div class="form-group">
-                            <label for="pwd">Confirm Password:</label>
-                            <input name="confirm_password"  type="password" class="form-control" id="confirm_password">
-                          </div>
-                          <button type="submit" class="btn btn-primary">Update Password</button>
-                        </form>
-                </div>
+                    @if(session()->get('success'))
+                      <div class="alert alert-success">
+                        {{ session()->get('success') }}  
+                      </div>
+                    @endif
+                    @if(session()->get('error'))
+                      <div class="alert alert-danger">
+                        {{ session()->get('error') }}  
+                      </div>
+                    @endif
+
+                            <div class="review-tab-pro-inner">
+                                <ul id="myTab3" class="tab-review-design">
+                                    <li class="active"><a href="#update_profile"><i class="fa fa-pencil" aria-hidden="true"></i> Update Profile</a></li>
+                                    <li><a href="#update_pass"><i class="fa fa-pencil" aria-hidden="true"></i> Change Password</a></li>
+                                </ul>
+                                <div id="myTabContent" class="tab-content custom-product-edit">
+                                    <div class="product-tab-list tab-pane fade active in" id="update_profile">
+                                         {!! Form::open(['url' => route('updateprofile'), 'class' => 'update-profile']) !!}
+                                            {{ csrf_field() }}
+                                        <div class="row">
+                                            <div class="col-lg-6 col-md-6 col-sm-6 col-xs-12">
+                                                <div class="review-content-section">
+                                                     <label for="first_name">First Name:</label>
+                                                    <div class="input-group mg-b-pro-edt">
+                                                        <span class="input-group-addon"><i class="fa fa-user" aria-hidden="true"></i></span>
+                                                        <input type="text" class="form-control" id="first_name" name="first_name" value="{{$user->first_name}}">
+                                                    </div>
+                                                    <label for="last_name">Last Name:</label>
+                                                    <div class="input-group mg-b-pro-edt">
+                                                        <span class="input-group-addon"><i class="fa fa-pencil" aria-hidden="true"></i></span>
+                                                         <input type="text" class="form-control" id="last_name" name="last_name" value="{{$user->last_name}}">
+                                                    </div>
+                                                </div>
+                                            </div>
+                                            <div class="col-lg-6 col-md-6 col-sm-6 col-xs-12">
+                                                <div class="review-content-section">
+                                                    <label for="email">Email:</label>
+                                                    <div class="input-group mg-b-pro-edt">
+                                                        <span class="input-group-addon"><i class="fa fa-user" aria-hidden="true"></i></span>
+                                                        <input type="email" class="form-control" id="email" name="email" value="{{$user->email}}" disabled>
+                                                    </div>
+                                                </div>
+                                            </div>
+                                        </div>
+                                        <div class="row">
+                                            <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
+                                                <div class="text-center mg-b-pro-edt custom-pro-edt-ds">
+                                                    <button type="submit" class="btn btn-primary waves-effect waves-light m-r-10">Save
+                                                    </button>
+                                                </div>
+                                            </div>
+                                        </div>
+                                        </form>
+                                    </div>
+
+                                    <div class="product-tab-list tab-pane fade" id="update_pass">
+                                         {!! Form::open(['url' => route('updatepassword'), 'class' => 'update-password']) !!}
+                                            {{ csrf_field() }}
+                                        <div class="row">
+                                            <div class="col-lg-6 col-md-6 col-sm-6 col-xs-12">
+                                                <div class="review-content-section">
+                                                    <label for="new_password">New Password:</label>
+                                                    <div class="input-group mg-b-pro-edt">
+                                                        <span class="input-group-addon"><i class="fa fa-pencil" aria-hidden="true"></i></span>
+                                                          <input name="new_password" type="password" class="form-control" id="new_password">
+                                                    </div>
+                                                </div>
+                                            </div>
+                                            <div class="col-lg-6 col-md-6 col-sm-6 col-xs-12">
+                                                <div class="review-content-section">
+                                                    <label for="confirm_password">Confirm Password:</label>
+                                                    <div class="input-group mg-b-pro-edt">
+                                                        <span class="input-group-addon"><i class="fa fa-user" aria-hidden="true"></i></span>
+                                                        <input name="confirm_password"  type="password" class="form-control" id="confirm_password">
+                                                    </div>
+                                                </div>
+                                            </div>
+                                        </div>
+                                        <div class="row">
+                                            <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
+                                                <div class="text-center mg-b-pro-edt custom-pro-edt-ds">
+                                                    <button type="submit" class="btn btn-primary waves-effect waves-light m-r-10">Update
+                                                    </button>
+                                                </div>
+                                            </div>
+                                        </div>
+                                        </form>
+                                    </div>
+
+                                </div>
+                            </div>
+                        </div>
             </div>
         </div>
     </div>
